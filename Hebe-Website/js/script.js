1bace7cfade69e9bf66449acbccd1f90a5a185e3
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-03-05 18:44:55 +0900

    로그인/회원가입 페이지 추가 및 연결

---
+++ Hebe-Website/js/script.js
@@ -0,0 +1,13 @@
+
+
+function Login(){
+    document.getElementById('login').style.left = "5vmin";
+    document.getElementById('Registration').style.left =  "50vmin";
+    document.getElementById('btn').style.left = "0vmin";
+}
+
+function Registr(){
+    document.getElementById('login').style.left = "-50vmin";
+    document.getElementById('Registration').style.left =  "5vmin";
+    document.getElementById('btn').style.left = "14vmin";
+}
