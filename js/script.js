bb521f648fb27e2f58e054401d07bf7bc0be6bb1
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-03-11 15:15:22 +0900

    메뉴 버튼 완료 및 찾아요 메인 완성

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
 
 
 function Login(){
-    document.getElementById('login').style.left = "5vmin";
-    document.getElementById('Registration').style.left =  "50vmin";
-    document.getElementById('btn').style.left = "0vmin";
+    if( document.documentElement.clientWidth >834){
+        document.getElementById('login').style.left = "5vmin";
+        document.getElementById('Registration').style.left =  "50vmin";
+        document.getElementById('btn').style.left = "0vmin";
+        document.getElementById('btn').style.marginTop= "0vmin";
+
+    }else{
+        document.getElementById('login').style.left = "5vmin";
+        document.getElementById('Registration').style.left =  "50vmin";
+        document.getElementById('btn').style.marginTop= "0vmin";
+    }
 }
 
 function Registr(){
-    document.getElementById('login').style.left = "-50vmin";
-    document.getElementById('Registration').style.left =  "5vmin";
-    document.getElementById('btn').style.left = "14vmin";
+    if(document.documentElement.clientWidth >834){
+        document.getElementById('login').style.left = "-50vmin";
+        document.getElementById('Registration').style.left =  "5vmin";
+        document.getElementById('btn').style.left = "14vmin";
+        document.getElementById('btn').style.marginTop= "0vmin";
+    }else{
+        document.getElementById('login').style.left = "-50vmin";
+        document.getElementById('Registration').style.left =  "5vmin";
+        document.getElementById('btn').style.marginTop = "5.2vmin";
+    }
+}
+
+let MeneTrueFalse =0;
+let MeneTrueFalseText ='';
+function MenuOpenClose(){
+    MeneTrueFalse ++;
+    MeneTrueFalse%2==0 ? MeneTrueFalseText='none' : MeneTrueFalseText='block';
+ 
+    let Mene = document.getElementsByClassName('Menu-bar');
+        for(var i=0; i<Mene.length; i++){
+            Mene[i].style.display=MeneTrueFalseText;
+    }
 }
