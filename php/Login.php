104f99cf4993950f655e0ce86088721fbbf4e656
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-03-17 15:09:58 +0900

    인식은 고쳤는데 다른거 어떻게 고칠지 고민중

--- php/Login.php
+++ php/Login.php
@@ -1,17 +1,19 @@
 <?php
-
+    session_start();
     header("Content-Type:text/html; charset=UTF-8");
     $conn = new mysqli("localhost","HeBe","hebeqlalfqjsgh","hebe");
     mysqli_query($conn,'SET NAMES utf8');
 
-    $id= $_POST['User_LoginId'];
-    $Password= $_POST['LoginPassword'];
-    if($id=="" ||  $Password==""){
+    $id= $_GET['User_Id'];
+    $Password= $_GET['Password'];
+    
+
+    if($id==""||  $Password="" ){
         echo '<script>alert("아이디나 패스워드를 입력하세요"); location.href="../Login.html";</script> ';
     }else{
          
         $sql = "select * from MemberJoin where User_Id='$id'";
-        $member = $sql->fetch_array();
+        $member = mysqli_fetch_array($sql);
         $hash_pw = $member['LoginPassword'];
 
         if(password_verify($Password,$hash_pw )){
