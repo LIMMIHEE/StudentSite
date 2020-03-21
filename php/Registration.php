aa09b348fbab6b53e42b91b8d3bd2442cdaddc42
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-03-21 21:05:55 +0900

    .하나의 중요성^^

--- php/Registration.php
+++ php/Registration.php
@@ -1,14 +1,14 @@
 <?php
-    session_start();
+
     header("Content-Type:text/html; charset=UTF-8");
-    $conn = new mysqli("localhost","HeBe","hebeqlalfqjsgh","hebe");
+    $conn = new mysqli("localhost","root","apmsetup","hebe");
     mysqli_query($conn,'SET NAMES utf8');
 
     $id= $_GET['User_Id'];
     $email= $_GET['email'];
     $Password= $_GET['Password'];
 
-    $sql="insert into MemberJoin(User_Id,email,Password) values 
+    $sql="insert into memberjoin(User_id,email,Password) values 
     ('$id','$email','$Password')";
 
     $res = $conn->query($sql);
